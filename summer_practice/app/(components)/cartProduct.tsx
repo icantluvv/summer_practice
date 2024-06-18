21f32efd8e3f13e54602b32ece1d7ef041821221
Sergey <99052479+icantluvv@users.noreply.github.com>
2024-06-18 22:56:24 +0300

    fix responsivity

--- summer_practice/app/(components)/cartProduct.tsx
+++ summer_practice/app/(components)/cartProduct.tsx
@@ -3,50 +3,52 @@ import Image from "next/image"
 
 const CartProduct = () => {
     return (
-        <div className="w-full flex">
-            <ul className="flex text-[16px] items-center flex-1">
-                <li className="w-[40%] pl-[2em] flex lg:flex-row flex-col lg:gap-x-6 gap-y-4 lg:items-center">
-                    <div className="max-w-[107px]">
+        <div className="w-full flex  mt-6 border-b pb-5">
+            <div className="flex text-[16px] items-center w-full lg:flex-row flex-col relative">
+                <div className="lg:w-[50%] w-[100%] px-2 flex lg:flex-row flex-col lg:gap-x-6 gap-y-4 items-center  ">
+                    <div className="lg:max-w-[107px] w-[30%] lg:min-w-[107px] min-w-[170px]">
                         <Image
                             src={"/images/cart_prod_image.svg"}
                             width={107}
                             height={107}
                             alt={""}
                             layout="responsive"
+                            className="lg:max-w-[107px] w-[300px] "
                         ></Image>
                     </div>
-                    <span className="w-[80%]">
+                    <span>
                         <p>Spider-Man & Miles</p>
                     </span>
-                </li>
-                <li className="w-[10%]">
-                    <span>
+                </div>
+                <div className="lg:w-[30%] w-full">
+                    <span className="flex justify-center">
                         <p>7599p</p>
                     </span>
-                </li>
-                <li className="w-[20%]">
+                </div>
+                <div className="lg:w-[14%] w-[50%] lg:max-w-full max-w-[160px] flex justify-around items-center">
+                    <button className="w-[30px] h-[30px] border border-[#e9e9e9] text-[#a9a9a9] flex items-center justify-center rounded-[5px] active:bg-[#e9e9e9]">
+                        -
+                    </button>
                     <span>
                         <p>1</p>
                     </span>
-                </li>
-                <li className="w-[30%] flex justify-between pr-[5%]">
-                    <span>
-                        <p>7599p</p>
-                    </span>
-                </li>
-            </ul>
-            <div className="pr-[5%] flex items-center">
-                <button>
-                    <div>
-                        <Image
-                            src={"/images/delete_product.svg"}
-                            width={107}
-                            height={107}
-                            alt={""}
-                            layout="responsive"
-                        ></Image>
-                    </div>
-                </button>
+                    <button className="w-[30px] h-[30px] border border-[#e9e9e9] text-[#a9a9a9] flex items-center justify-center rounded-[5px] active:bg-[#e9e9e9]">
+                        +
+                    </button>
+                </div>
+                <div className="absolute top-5 lg:h-full lg:top-0 right-[5%] lg:right-3 flex items-center">
+                    <button>
+                        <div>
+                            <Image
+                                src={"/images/delete_product.svg"}
+                                width={107}
+                                height={107}
+                                alt={""}
+                                layout="responsive"
+                            ></Image>
+                        </div>
+                    </button>
+                </div>
             </div>
         </div>
     )
--- summer_practice/app/cart/page.tsx
+++ summer_practice/app/cart/page.tsx
@@ -4,42 +4,35 @@ import CartProduct from "../(components)/cartProduct"
 
 const page = () => {
     return (
-        <main className="w-full min-h-[41em] lg:pt-0 pt-[100px] flex justify-center">
-            <section className="flex w-[90%] justify-center gap-6 my-[4em]">
-                <div className="flex flex-col w-[77%] max-w-[800px] ">
-                    <div className="bg-[#e8e8e8] w-full h-[55px] flex items-center mb-[5em]">
+        <main className="w-full min-h-[41em] lg:pt-0 pt-[100px] flex lg:justify-center">
+            <section className=" w-full lg:w-[90%] justify-between lg:justify-around items-center lg:items-start mt-[4em] flex lg:flex-row flex-col">
+                <div className="flex flex-col lg:w-[73%]  max-w-[800px] w-full">
+                    <div className="bg-[#e8e8e8] w-full h-[55px]  items-center lg:flex hidden">
                         <ul className="flex w-full font-semibold ">
-                            <li className="w-[40%] pl-[2em]">Продукт</li>
-                            <li className="w-[10%]">Цена</li>
-                            <li className="w-[20%]">Количество</li>
-                            <li className="w-[30%]">Итоговая цена</li>
+                            <li className="w-[50%] pl-4">Продукт</li>
+                            <li className="w-[30%] ">Цена</li>
+                            <li className="w-[20%] ">Количество</li>
                         </ul>
                     </div>
                     <CartProduct></CartProduct>
                 </div>
-                <div className="flex flex-col w-[35%] max-w-[350px] h-[350px] bg-[#e8e8e8] items-center pt-8">
-                    <span>
-                        <h1 className="font-semibold text-[32px]">
-                            Cart Totals
-                        </h1>
-                    </span>
-                    <span className="w-[70%] flex flex-col items-center gap-8 mt-[4em]">
-                        <span className="font-medium text-[16px] flex justify-between w-full">
-                            Subtotal
-                            <p className="text-[#9F9F9F] text-[16px]">
-                                Rs. 250,000.00
-                            </p>
-                        </span>
-                        <span className="font-medium text-[16px] flex justify-between w-full">
-                            Total
-                            <p className="text-[20px] text-red-500">
-                                Rs. 250,000.00
-                            </p>
-                        </span>
-                    </span>
-                    <button className="font-medium active:bg-[#CECECE] hover:bg-[#d9d9d9] bg-transparent text-[20px] border-2 border-black mt-[45px] px-10 py-2 rounded-2xl">
-                        Check Out
+                <div className="flex flex-col-reverse mb-5 lg:flex-col w-[98%] lg:w-[24%] mt-[5em] lg:mt-0">
+                    <button className="w-full h-[60px] bg-[#e8e8e8] lg:bg-black text-black lg:text-white rounded-[10px] text-[20px] font-semibold flex items-center justify-center hover:bg-[#d9d9d9] hover:text-black active:bg-[#b8b8b8] transition ">
+                        Check out
                     </button>
+
+                    <div className="mt-6 w-full h-[100px] mb-1 bg-black text-white lg:text-black lg:bg-[#e8e8e8] rounded-[10px] text-[20px] font-semibold flex items-center justify-center  ">
+                        <div className="w-full flex flex-col items-center text-[16px] font-normal">
+                            <span className="flex w-[90%] justify-between ">
+                                <p>Всего товаров: 1</p>
+                                <p>7599р</p>
+                            </span>
+                            <span className="flex w-[90%] justify-between ">
+                                <p>Выгода:</p>
+                                <p>0р</p>
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
